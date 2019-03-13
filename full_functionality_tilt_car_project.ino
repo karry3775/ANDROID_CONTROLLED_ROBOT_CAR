12a786660158d544b48d4b6130ef686f5819cf3d
karry3775 <43415631+karry3775@users.noreply.github.com>
2019-03-12 20:05:05 -0500

    Create full_functionality_tilt_car_project.ino

---
+++ full_functionality_tilt_car_project.ino
@@ -0,0 +1,101 @@
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+int data;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  Serial.println("Hi I am Arduino");
+
+}
+
+void forward() {
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  Serial.println("Moving forward");
+}
+
+void back() {
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  Serial.println("Moving backward");
+}
+
+void right() {
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+  Serial.println("turning right");
+}
+
+void left() {
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+  Serial.println("turning left");
+}
+
+void stopit() {
+  digitalWrite(ENA,HIGH);
+  digitalWrite(ENB,HIGH);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  Serial.println("stopping");
+}
+
+void loop() {
+  while(Serial.available()){
+    data = Serial.read();
+  }
+    
+    if (data=='1'){
+      forward();
+      Serial.println("Inside the forward if");
+    }
+    else if (data=='2'){
+      back();
+      Serial.println("Inside the back if");
+    }
+    else if (data=='0'){
+      stopit();
+      Serial.println("Inside the stopit if");
+    }
+    else if (data=='3'){
+      right();
+      Serial.println("Inside the right if");
+    }
+    else if (data=='4'){
+      left();
+      Serial.println("Inside the left if");
+    }
+    delay(1000); //delaying 3 seconds
+    stopit();
+   
+
+}
